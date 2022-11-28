4d54c98292abc4b2f95baf8659d7f0f02bdd785e
Sunil Kumar <sunil.kumar@bharatzup.com>
2022-11-28 21:28:17 +0530

    1.0.5

--- sunilLib/src/main/java/com/eagle/offline/music/player/kotlingreen/RajActivity.kt
+++ sunilLib/src/main/java/com/eagle/offline/music/player/kotlingreen/RajActivity.kt
@@ -1,15 +1,24 @@
 package com.eagle.offline.music.player.kotlingreen
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class RajActivity : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_raj)
-        object {
-            val sunil = "sunil"
-        }
+
+        val ss:String = intent.getStringExtra("Username").toString()
+        val tv = findViewById<TextView>(R.id.txt)
+        tv.text = ss
 
     }
+}
+
+
+object kumar{
+    val sunil = "sunil"
 }
---
+++ sunilLib/src/main/java/com/eagle/offline/music/player/kotlingreen/getDigitalConnectDatabase.kt
@@ -0,0 +1,6 @@
+package com.eagle.offline.music.player.kotlingreen
+
+object getDigitalConnectDatabase {
+
+    val sunil = "sunil kumar"
+}
