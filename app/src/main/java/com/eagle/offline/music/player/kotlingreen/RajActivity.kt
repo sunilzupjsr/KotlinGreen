70fe8be54d63d62f84f76af61102561a6d1e6a28
Sunil Kumar <sunil.kumar@bharatzup.com>
2022-11-28 20:13:51 +0530

    new repo

--- app/src/main/java/com/eagle/offline/music/player/kotlingreen/RajActivity.kt
+++ app/src/main/java/com/eagle/offline/music/player/kotlingreen/RajActivity.kt
@@ -3,9 +3,13 @@ package com.eagle.offline.music.player.kotlingreen
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class RajActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_raj)
+        object {
+            val sunil = "sunil"
+        }
+
     }
 }
